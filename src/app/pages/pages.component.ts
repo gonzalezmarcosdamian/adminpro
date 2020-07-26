a90f4b642273ffa75e1c65d849501b8582e90853
Marcos damian Gonzalez <ingonzalezdamian@gmail.com>
2020-07-25 21:13:25 -0300

    theme check

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
-
-  constructor() { }
+  public linkTheme = document.querySelector('#theme');
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const url =
+      localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
 
+    this.linkTheme.setAttribute('href', url);
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -16,6 +16,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   exports: [
     DashboardComponent,
