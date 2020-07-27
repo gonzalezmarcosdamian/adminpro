8ef6013e4141f380b6ff8f420c6442598330b272
Marcos damian Gonzalez <ingonzalezdamian@gmail.com>
2020-07-26 21:28:44 -0300

    services 1ro

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,3 +1,4 @@
+import { SettingsService } from './../../services/settings.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,37 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
-  public links: NodeListOf<Element>;
-
-  constructor() {}
+  constructor(private SettingsService: SettingsService) {}
 
   ngOnInit(): void {
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    this.SettingsService.checkCurrentTheme();
   }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme.setAttribute('href', url);
-
-    localStorage.setItem('theme', url);
-
-    this.checkCurrentTheme();
-  }
-
-  checkCurrentTheme() {
-    this.links.forEach((elem) => {
-      elem.classList.remove('working');
-
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme.getAttribute('href');
-
-      if (btnThemeUrl === currentTheme) {
-        elem.classList.add('working');
-      }
-    });
+    this.SettingsService.changeTheme(theme);
   }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,3 +1,4 @@
+import { SettingsService } from './../services/settings.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,13 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class PagesComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
-  constructor() {}
+  //const serv
+  constructor(private SettingsService: SettingsService) {}
 
-  ngOnInit(): void {
-    const url =
-      localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
-
-    this.linkTheme.setAttribute('href', url);
-  }
+  ngOnInit(): void {}
 }
